527150bcfc0b56d1ff519d92480b47d2e0cdbab8
LucidDion <lucid.dion@gmail.com>
2021-10-25 12:55:12 -0400

    boilerplate for Demo Extension class

---
+++ WL7ExtensionDemos/DemoExtension.cs
@@ -0,0 +1,9 @@
+﻿using WealthLab.WPF;
+
+namespace WL7ExtensionDemos
+{
+    //Our WL7 ExtensionBase class, which provides new menu items to WL7's Extension menu, and can inject content into the Help system and QuickRef
+    public class DemoExtension : WL7ExtensionBase
+    {
+    }
+}
