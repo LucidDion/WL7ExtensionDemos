d6a7ec732db49d40847837e69403983c9358835c
LucidDion <lucid.dion@gmail.com>
2021-10-25 13:15:51 -0400

    open a child window

---
+++ WL7ExtensionDemos/ChildWindows/DemoChildWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using WealthLab.WPF;
+
+namespace WL7ExtensionDemos
+{
+    //This child window will appear when the "Demo" menu item is selected under the WL7 Extensions menu
+    public partial class DemoChildWindow : ChildWindow
+    {
+        //constructor
+        public DemoChildWindow()
+        {
+            InitializeComponent();
+        }
+
+        //a "Token" is used for internal tracking of the Child Window, specifically when saving and restoring Workspaces
+        public override string Token => "Demo";
+    }
+}
--- WL7ExtensionDemos/DemoExtension.cs
+++ WL7ExtensionDemos/DemoExtension.cs
@@ -1,9 +1,30 @@
-﻿using WealthLab.WPF;
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using WealthLab.WPF;
+using System.Windows;
 
 namespace WL7ExtensionDemos
 {
     //Our WL7 ExtensionBase class, which provides new menu items to WL7's Extension menu, and can inject content into the Help system and QuickRef
     public class DemoExtension : WL7ExtensionBase
     {
+        //Initialize
+        public override List<MenuItem> Initialize()
+        {
+            //create and return a menu item that will open our ChildWindow
+            List<MenuItem> mi = new List<MenuItem>();
+            MenuItem mni = CreateExtensionMenuItem("Demo Extension", Properties.Resources.Demo, mniClick);
+            mi.Add(mni);
+            return mi;
+        }
+
+        //private members
+
+        //extension menu item was clicked, create our ChildWindow and make it appear in WL7
+        private void mniClick(object sender, RoutedEventArgs e)
+        {
+            DemoChildWindow dcw = new DemoChildWindow();
+            MyClientHost.ShowExtensionChildWindow(dcw, "Demo ChildWindow", Properties.Resources.Demo);
+        }
     }
 }
